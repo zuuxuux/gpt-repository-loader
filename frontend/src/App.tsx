6a9c5d199f13799c6b129c4c21ba419f9426a1f1
zuuxuux <zuuxuux@gmail.com>
2025-01-07 20:37:06 +0000

    Making it look better

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,30 @@
 // src/App.tsx
 import ChatContainer from './components/ChatContainer'
-import { NavBar } from './components/NavBar'
 import TopicCard from './components/TopicCard'
 import { mockTopics } from './data/topics'
 import './App.css'
 
-// If you’re using the modular approach from earlier, 
-// import SideNav from "@/components/ui/SideNav/SideNav" (or wherever you put it).
-import SideNav from '@/components/SideNav'  
+// The hierarchical nav we want to place on the right
+import SideNav from './components/SideNav'
 
 function App() {
   return (
-    <div className="flex">
-      {/* MAIN CONTENT ON THE LEFT */}
-      <div className="flex-1">
-        {/* Existing content */}
-        <div className="container mx-auto">
-          <ChatContainer />
-        </div>
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-          {mockTopics.map((topic, index) => (
-            <TopicCard
-              key={index}
-              topic={topic}
-              onThumbsUp={() => console.log(`Liked: ${topic.title}`)}
-              onThumbsDown={() => console.log(`Disliked: ${topic.title}`)}
-            />
-          ))}
-        </div>
+    <div>
+      <div className="container mx-auto">
+        <ChatContainer />
+      </div>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
+        {mockTopics.map((topic, index) => (
+          <TopicCard
+            key={index}
+            topic={topic}
+            onThumbsUp={() => console.log(`Liked: ${topic.title}`)}
+            onThumbsDown={() => console.log(`Disliked: ${topic.title}`)}
+          />
+        ))}
       </div>
 
-      {/* SIDENAV ON THE RIGHT */}
+      {/* Fixed Nav on the Right */}
       <SideNav />
     </div>
   )
